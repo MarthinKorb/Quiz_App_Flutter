68f7efcd8515dd82e647cd6eb47c28008154827c
Marthin Korb <marthinkorb@gmail.com>
2020-11-15 14:51:10 -0300

    adding switch

--- lib/result.dart
+++ lib/result.dart
@@ -8,13 +8,26 @@ class Result extends StatelessWidget {
   Result(this.score, this.onRestartQuiz);
 
   String get resultPhrase {
-    if(score == 0) {
-      return 'Deu ruim!';
-    } else if (score < 3){
-      return 'Você foi bem!';
-    } else {
-      return 'Tu é o Jiraya da porra toda!';
+
+    switch (score) {
+      case 0:
+        return 'Deu ruim!'; 
+      case 1:
+        return 'Uma é melhor que nada!';
+      case 2:
+        return 'Duas está bom!';
+      case 3:
+      return 'Excelente! Acertou todas!';
+        break;
+      default:
     }
+    // if(score == 0) {
+    //   return 'Deu ruim!';
+    // } else if (score < 3){
+    //   return 'Você foi bem!';
+    // } else {
+    //   return 'Tu é o Jiraya da porra toda!';
+    // }
   }
 
   @override
